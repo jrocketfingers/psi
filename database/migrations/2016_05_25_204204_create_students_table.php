b209c9e41d0016f4a69272f21eea0d9e68814cdb
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-25 23:36:00 +0200

    Created students table

---
+++ database/migrations/2016_05_25_204204_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->integer('id')->unsigned();
+            $table->primary('id');
+
+            $table->foreign('id')->references('id')->on('users')->onUpadate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
