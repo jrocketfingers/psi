9affe53236c70bb4aa5669b81a92f5317fc407e9
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-25 23:35:27 +0200

    Created teams_roles table

---
+++ app/TeamStudent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TeamStudent extends Model
+{
+    protected $table = 'teams_students';
+}
---
+++ database/migrations/2016_05_25_185147_create_teams_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams_roles', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('team_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->integer('degree');
+            
+            $table->foreign('team_id')->references('id')->on('teams')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teams_roles');
+    }
+}
