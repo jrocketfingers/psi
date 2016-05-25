dd3de777d48f016dfb546515b9fe40a418c311fb
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-25 23:36:55 +0200

    Created students_roles table

---
+++ database/migrations/2016_05_25_205415_create_students_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('role_id')->unsigned();
+            $table->integer('students_id')->unsigned();
+            $table->boolean('isLeader');
+
+            $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('students_id')->references('id')->on('students')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students_roles');
+    }
+}
