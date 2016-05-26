35877bd520170aea8b703e8fa9f128ea207d5dfb
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-26 19:28:41 +0200

    Added My roles link

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ $user->name }}
-    {{ $user->email }}
+    <div>{{ $user->name }}</div>
+    <div>{{ $user->email }}</div>
+    <div><a href="{{ action('UsersController@edit', [$user->id]) }}">Edit</a> </div>
+    @if(\App\Student::isStudent($user->id))
+        <div>
+            <a href="{{ url('/students_roles/'.$user->id)}}">My roles</a>
+        </div>
+    @endif
 @endsection
