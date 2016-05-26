b54fd3321fc2121faa278f9a88f018a2c7bb6369
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-26 17:14:08 +0200

    Added delete button to allow admins to delet users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($users as $user)
-        <div>{{ $user->name }}</div>
-    @endforeach
+    <table>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->name }}</td>
+                <td>
+                    <form class="form-control" role="form" method="POST" action="{{ url('users/destroy/'.$user->id) }}" >
+                        {!! csrf_field() !!}
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
