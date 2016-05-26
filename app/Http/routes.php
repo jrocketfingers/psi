4a92c1cb4e2bb13d821c6809b7bcc416fd36d23a
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-26 12:01:21 +0200

    Added a post route to UsersController:destroy

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index');
 Route::get('/tasks', 'HomeController@index');
 
 Route::resource('users', 'UsersController');
+
+Route::post('/users/destroy/{id}', 'UsersController@destroy');
