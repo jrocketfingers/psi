3ea6098843700f4820d192f98a969de8624eca3d
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-26 19:27:30 +0200

    Changed StudentRole model

--- app/StudentRole.php
+++ app/StudentRole.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class StudentRole extends Model
 {
     protected $table = 'students_roles';
+
+    protected $fillable = [
+        'student_id', 'role_id'
+    ];
 }
