c2f097fd1e5ee41ec2e09bb8b26500a67535a491
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-26 19:43:30 +0200

    Added delete button in index view for StudentsRolesController

--- resources/views/students_roles/index.blade.php
+++ resources/views/students_roles/index.blade.php
@@ -8,6 +8,12 @@
         @foreach($roles as $role)
             <div class="row">
                 <div class="col-sm-4">{{ $role->name }}</div>
+                <div class="col-sm-4">
+                    <form class="form-group" role="form" method="POST" action="{{ url('/students_roles/destroy/'.$role->id ) }}">
+                        {{ csrf_field() }}
+                        <input type="submit" value="Delete">
+                    </form>
+                </div>
             </div>
         @endforeach
     </div>
