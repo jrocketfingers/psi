33c9885882de81001625161c34c9a4d40e8d7922
simke9445 <simke9445@gmail.com>
2016-05-27 15:24:10 +0200

    Add team list view on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\Team;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $teams = Team::all();
+        return view('home', [
+            'teams' => $teams
+        ]);
     }
 }
