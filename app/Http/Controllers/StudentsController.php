b578109e11a327be9f0b85aa54c0d290f6857efa
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-27 19:25:11 +0200

    Changed show method of StudentsController

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\StudentRepository;
 use App\Student;
 use App\User;
 use Illuminate\Http\Request;
@@ -11,11 +12,11 @@ use App\Http\Requests;
 class StudentsController extends Controller
 {
     public function __construct() {
-        $this->middleware('auth');
+        $this->middleware('assistant');
     }
 
     public function getAll() {
-        $students = Student::getAll();
+        $students = StudentRepository::getAll();
 
         return view('students.index', [
             'students' => $students,
@@ -23,10 +24,23 @@ class StudentsController extends Controller
     }
 
     public function getByRole($role_id) {
-        $students = Student::getByRole($role_id);
+        $students = StudentRepository::getByRole($role_id);
 
         return view('students.index', [
            'students' => $students
+        ]);
+    }
+    
+    public function show($student_id) {
+        $user = User::find($student_id);
+        $student = Student::find($student_id);
+        $team = null;
+        if($student->team_id) {
+            $team = Team::find($student->team_id);
+        }
+        return view('students.details', [
+            'user' => $user,
+            'team' => $team,
         ]);
     }
 }
