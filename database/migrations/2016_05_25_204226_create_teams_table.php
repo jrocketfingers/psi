23c434582f4ce8931dc02129886e49cab1b1b9f0
simke9445 <simke9445@gmail.com>
2016-05-27 15:22:32 +0200

    Add reordered migrations

---
+++ database/migrations/2016_05_25_204226_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('project_name');
+            $table->string('description');
+            $table->integer('leader_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('leader_id')->references('id')->on('students')->onUpadate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teams');
+    }
+}
