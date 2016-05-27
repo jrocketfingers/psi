fba5186eb469f721d754fb455135862a78fcaa3b
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-27 17:43:47 +0200

    Added removeRole and doesExist methods

--- app/StudentRole.php
+++ app/StudentRole.php
@@ -9,6 +9,24 @@ class StudentRole extends Model
     protected $table = 'students_roles';
 
     protected $fillable = [
-        'student_id', 'role_id'
+        'student_id', 'role_id', 'isActive'
     ];
+
+    public static function doesExist($student_id, $role_id) {
+        $student_role = StudentRole::where('student_id', '=', $student_id)
+                                    ->where('role_id', '=', $role_id)
+                                    ->first();
+
+        if($student_role === null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static function removeRole($student_id, $role_id) {
+        StudentRole::where('student_id', '=', $student_id)
+                    ->where('role_id', '=', $role_id)
+                    ->delete();
+    }
 }
