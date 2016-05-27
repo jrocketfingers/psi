293d323369ed14d1d81c19e816247eca0ed054f5
simke9445 <simke9445@gmail.com>
2016-05-27 15:27:33 +0200

    Add team & student factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,6 +10,10 @@
 | database. Just tell the factory how a default model should look.
 |
 */
+use App\User;
+use App\Role;
+use App\Team;
+use App\Student;
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
@@ -18,4 +22,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
+});
+
+$factory->define(App\Student::class, function(Faker\Generator $faker){
+	return [
+		'id' => factory(User::class)->create()->id,
+	];
+});
+
+$factory->define(App\Team::class, function(Faker\Generator $faker){
+	$student = factory(Student::class)->create();
+	$student->roles()->save($faker->randomElements(Role::all()->toArray(), 5));
+	return [
+		'name' => $faker->name,
+		'project_name' => $faker->project_name,
+		'description' => $faker->description,
+		'leader_id' => $student->id,
+	];
 });
