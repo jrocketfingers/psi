005aeb9e4d136f24b7384ba5c1e63dd5138cf124
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-27 19:26:54 +0200

    Added details button to index view of StudentsController

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -7,6 +7,9 @@
                 <div class="col-sm-4">
                     {{ $student->name }}
                 </div>
+                <div class="col-sm-4">
+                    <a href="{{ url('/students/show/'.$student->id) }}">Details</a>
+                </div>
             </div>
         @endforeach
     </div>
