fa15e6aa5dc70bf4ce91acc1c85a988824797a8d
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-29 15:13:13 +0200

    Added implemeted methods

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -45,5 +45,36 @@ class User implements Authenticatable {
         return $this->id;
     }
 
+    public function getAuthIdentifierName()
+    {
+        // TODO: Implement getAuthIdentifierName() method.
+    }
+
+    public function getAuthIdentifier()
+    {
+        // TODO: Implement getAuthIdentifier() method.
+    }
+
+    public function getAuthPassword()
+    {
+        // TODO: Implement getAuthPassword() method.
+    }
+
+    public function getRememberToken()
+    {
+        // TODO: Implement getRememberToken() method.
+    }
+
+    public function setRememberToken($value)
+    {
+        // TODO: Implement setRememberToken() method.
+    }
+
+    public function getRememberTokenName()
+    {
+        // TODO: Implement getRememberTokenName() method.
+    }
+
+
 }
 
