3efba99d9a8e6e49e6082bec1e5932a060f7478a
simke9445 <simke9445@gmail.com>
2016-05-29 20:48:14 +0200

    Add User show View representation

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,12 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>{{ $user->name }}</div>
-    <div>{{ $user->email }}</div>
-    <div><a href="{{ action('UsersController@edit', [$user->id]) }}">Edit</a> </div>
-    @if(\App\Student::isStudent($user->id))
-        <div>
-            <a href="{{ url('/students_roles/'.$user->id)}}">My roles</a>
-        </div>
-    @endif
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-8 col-lg-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<div class="row">
+						<div class="col-lg-11">
+							<h3 class="panel-title">Profile Info</h3>
+						</div>
+						<div class="col-lg-1">
+							<a href="{{ action('UsersController@edit', [$user->id]) }}">Edit</a>
+						</div>
+					</div>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="row">
+								<div class="col-lg-4 col-lg-offset-2">
+									<span>Username: </span>
+								</div>
+								<div class="col-lg-4 col-lg-offset-2"> 
+									<div> {{ $user->name }} </div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-lg-4 col-lg-offset-2">
+									<span>Email: </span>
+								</div>
+								<div class="col-lg-4 col-lg-offset-2"> 
+									<div> {{ $user->email }} </div>
+								</div>
+							</div>
+							
+						</div>
+						@if(\App\Student::isStudent($user->id))
+							<div class="col-lg-6 col-lg-offset-2">
+								<div class="row" style="text-align: center;">
+									@foreach($roles as $role)
+							            <div class="col-lg-4 text-center">
+							                <div class="label label-default"> {{ $role->name }} </div>
+							            </div>
+							        @endforeach
+								</div>
+							</div>
+						@endif	
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
+						    
