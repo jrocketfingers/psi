da86141abf70ac5760f42ec234077d4e3916a9ed
simke9445 <simke9445@gmail.com>
2016-05-29 20:44:20 +0200

    Change fillable fields in Student Model

--- app/Student.php
+++ app/Student.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $fillable = [
-        'id',
+        'name',
+        'email',
     ];
 
     public static function isStudent($id) {
