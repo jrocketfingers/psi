3d3fcb1d580a9bb48c68d689cdc3fc868f144f80
simke9445 <simke9445@gmail.com>
2016-05-29 20:42:26 +0200

    Add show/edit/update actions to StudentsController

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Student;
 use App\User;
+use App\Team;
+use App\Role;
+use App\StudentRole;
+use App\Repositories\RolesRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collections;
 
 use App\Http\Requests;
 
@@ -12,6 +17,65 @@ class StudentsController extends Controller
 {
     public function __construct() {
         $this->middleware('auth');
+    }
+
+    public function index($id = null)
+    {
+
+        $teams = Team::all();
+
+        return view('students.index', [
+            'teams' => $teams,
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $student = User::find($id);
+        $personal_roles = array_column(RolesRepository::getByStudentId($id), 'name', 'id');
+        $roles = array_column(Role::all()->toArray(), 'name', 'id');
+
+        
+        $missing_roles = array_diff($roles, $personal_roles);
+        /*$missing_roles = collect()->pluck('name', 'id');
+        $personal_roles = collect($personal_roles)->pluck('name', 'id');*/
+
+        return view('students.edit', [
+            'student' => $student,
+            'missing_roles' => $missing_roles,
+            'personal_roles' => $personal_roles,
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $input = $request->all();
+
+        $id = $request['id'];
+
+        $student = User::find($id);
+        $student->update($request->all());
+
+        $new_roles = Role::all()->only($request['add_role_id']);
+        $removed_roles = Role::all()->only($request['delete_role_id']);
+
+
+        foreach ($removed_roles as $role)
+        {
+            StudentRole::removeRole($student->id, $role->id);
+        }
+
+        foreach ($new_roles as $role)
+        {
+            StudentRole::create([
+                'student_id' => $student->id,
+                'role_id' => $role->id,
+            ]);
+        }
+        
+        $personal_roles = RolesRepository::getByStudentId($id);
+
+        return redirect()->action('StudentsController@show', [$id]);
     }
 
     public function getAll() {
@@ -27,6 +91,22 @@ class StudentsController extends Controller
 
         return view('students.index', [
            'students' => $students
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $roles = RolesRepository::getByStudentId($id);
+        $student = User::find($id);
+        return view('students.show', [
+            'student' => $student,
+            'roles' => $roles,
         ]);
     }
 }
