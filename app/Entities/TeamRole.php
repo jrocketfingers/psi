6ac79c40447615e542e37783d109165ac3b4767a
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-29 14:51:45 +0200

    Added TeamRole entity

--- app/Entities/TeamRole.php
+++ app/Entities/TeamRole.php
@@ -2,9 +2,62 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Doctrine\ORM\Mapping AS ORM;
 
-class TeamRole extends Model
+/*
+ * @ORM\Entity
+ * @ORM\Table(name = "teamrole")
+ */
+class TeamRole
 {
-    protected $table = 'teams_roles';
+    /*
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity = "Role", inversedBy = "teams")
+     * @ORM\JoinColumn(name = "role_id", referencedColumnName = "id")
+     */
+    private $role;
+
+    /*
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity = "Team", inversedBy = "roles")
+     * @ORM\JoinColumn(name = "team_id", referencedColumnName = "id")
+     */
+    private $team;
+
+    public function __construct($role, $team) {
+        $this->role = $role;
+        $this->team = $team;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeam()
+    {
+        return $this->team;
+    }
+
+    /**
+     * @param mixed $team
+     */
+    public function setTeam($team)
+    {
+        $this->team = $team;
+    }
 }
