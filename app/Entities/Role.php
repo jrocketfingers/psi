c1227d9161cec7daf577e99d81c66f0f3fe5c10e
Jovan Djukic <djukicjovan56@gmail.com>
2016-05-29 14:46:36 +0200

    Added Role Entity

--- app/Entities/Role.php
+++ app/Entities/Role.php
@@ -1,12 +1,123 @@
 <?php
 
-namespace App;
+namespace App\Entities;
 
-use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
+use Doctrine\Common\Collections\ArrayCollection;
+
+/*
+ * @ORM\Entity
+ * @ORM\Table(name = "role")
+ */
+class Role
 {
-    protected $fillable = [
-        'name', 'description',
-    ];
+   /*
+    * @ORM\ID
+    * @ORM\GeneratedValue
+    * @ORM\Column(type = "integer")
+    */
+    private $id;
+
+    private $name;
+    private $descripton;
+
+    /*
+     * @ORM\OneToMany(targetEntity = "StudentRole", mappedBy = "role")
+     */
+    private $students;
+
+    /*
+     * @ORM\OneToMany(targetEntity = "TeamRole", mappedBy = "role")
+     */
+    private $teams;
+
+    public function __construct() {
+        $this->students = new ArrayCollection();
+        $this->teams = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescripton()
+    {
+        return $this->descripton;
+    }
+
+    /**
+     * @param mixed $descripton
+     */
+    public function setDescripton($descripton)
+    {
+        $this->descripton = $descripton;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStudentrole()
+    {
+        return $this->studentrole;
+    }
+
+    /**
+     * @param mixed $studentrole
+     */
+    public function setStudentrole($studentrole)
+    {
+        $this->studentrole = $studentrole;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeamrole()
+    {
+        return $this->teamrole;
+    }
+
+    /**
+     * @param mixed $teamrole
+     */
+    public function setTeamrole($teamrole)
+    {
+        $this->teamrole = $teamrole;
+    }
+
+
+
+
 }
+?>
