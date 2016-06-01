a39fce3c868ade1a9f71cccd4024f643ea0a7f31
simke9445 <simke9445@gmail.com>
2016-06-01 17:13:47 +0200

    Added more functionality to Student Index view like team roles etc

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('details')
 	<li><a href="{{ action('StudentsController@show', [Auth::user()->id]) }}" ><i class="fa fa-btn fa-info"></i>Details</a></li>
+
+	@if ($student->is_leader)
+		<li><a href="{{ url('students/team/delete') }}" ><i class="fa fa-btn fa-info"></i>Disband Team</a></li>
+	@else
+		<li><a href="{{ url('students/team/create') }}" ><i class="fa fa-btn fa-info"></i>Create Team</a></li>
+	@endif
+	
 @endsection
 
 @section('content')
@@ -11,10 +18,31 @@
 	            <div class="col-lg-8 col-lg-offset-2">
 	                <div class="panel panel-default">
 	                    <div class="panel-heading">
-	                        {{ $team->name }}
+		                    <div class="row">
+		                    	<div class="col-lg-2">
+			                    	{{ $team->name }}
+			                    </div>
+		                        
+		                        @foreach ($team->students as $student)
+		                        	<div class="col-lg-1 col-lg-offset-1">
+		                        		<label class="label label-default">
+			                        		{{ $student->user->name }}
+			                        	</label>
+		                        	</div>
+		                        @endforeach
+		                    </div>
+		                    
 	                    </div>
 	                    <div class="panel-body">
-	                        This is a Random Description
+	                        <p>This is a Random Description</p>
+
+	                        @foreach ($team->roles as $role)
+	                        	<div class="col-lg-1 col-lg-offset-1">
+	                        		<label class="label label-default">
+		                        		{{ $role->name }}
+		                        	</label>
+	                        	</div>
+	                        @endforeach
 	                    </div>
 	                </div>
 	            </div>
