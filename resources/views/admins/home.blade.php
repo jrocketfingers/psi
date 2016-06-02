30c988de79155e6d89914103a4315a780980787f
Jovan Djukic <djukicjovan56@gmail.com>
2016-06-02 11:29:05 +0200

    Changed admins home page.

--- resources/views/admins/home.blade.php
+++ resources/views/admins/home.blade.php
@@ -1,6 +1,6 @@
-@extends('home')
+@extends('layouts.app')
 
-@section('user_content')
+@section('content')
     <a href="{{ url('/admins/showAllUsers') }}">Link for editing users</a><br>
     <a href="{{ action('RolesController@index') }}">Link fore editing roles</a>
 @endsection
