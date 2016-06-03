556bebd302bd1f75f8b9ae0089f08e17fbb229ef
simke9445 <simke9445@gmail.com>
2016-06-03 14:32:08 +0200

    Add real-time notification update logic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,5 +100,46 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    <script type="text/javascript">
+
+        $(document).ready(function(){
+
+            (function poll() {
+               setTimeout(function() {
+                   $.get('http://192.168.99.100/students/notifications', function(data){
+                        var data = JSON.parse(data);
+                        var currentLength = $('ul#myMenu li').length;
+
+                        // $('#myMenu').empty();
+                        for (var i=0; i<data.length; i++)
+                        {
+                            $('#myMenu').prepend(
+                                $('<li>').append(
+                                    $('<span>').attr('class', 'label label-default').text(data[i].user_id)
+                            ));
+                        }
+
+                        $('#notify').text(data.length + currentLength);
+                        $('#notify').show();
+                        
+                    }).done(poll).fail(function(data){
+                        var w = window.open();
+                        $(w.document.body).html(data.responseText);
+                    });
+                }, 5000);
+            })();
+
+            $('#notifications').on('click', function(){
+
+                $('#notify').hide();
+
+            });
+
+
+        });
+
+    </script>
+
 </body>
 </html>
