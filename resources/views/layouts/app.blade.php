155cd6c2a8ef71c8379e7c868308a9e3900aa21c
simke9445 <simke9445@gmail.com>
2016-06-03 22:36:21 +0200

    Add rendering & operating logic for async notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,27 +114,75 @@
             (function poll() {
                setTimeout(function() {
                    $.get('http://192.168.99.100/students/notifications', function(data){
-                        var data = JSON.parse(data);
                         var currentLength = $('ul#myMenu li').length;
 
-                        // $('#myMenu').empty();
                         for (var i=0; i<data.length; i++)
                         {
                             $('#myMenu').prepend(
                                 $('<li>').append(
-                                    $('<span>').attr('class', 'label label-default').text(data[i].user_id)
+                                    $('<div>').attr('class', 'row').append(
+                                        $('<div>').attr('class', 'col-md-8').append(
+                                            $('<span>').attr('class', 'label label-default').text(data[i].text)
+                                        )
+                                    ).append(
+                                        $('<div>').attr('class', 'col-md-4').append(
+                                            $('<div>').attr({'class': 'fa fa-check', 'data-check': true, 'data-notification-id': data[i].id, 'name': 'notification'})
+                                        ).append(
+                                            $('<div>').attr({'class': 'fa fa-times', 'data-check': false, 'data-notification-id': data[i].id, 'name': 'notification'})
+                                        )
+                                    )
+                                    
                             ));
-                        }
 
-                        $('#notify').text(data.length + currentLength);
-                        $('#notify').show();
+                            $("ul#myMenu > li:first > div > div[class='col-md-4'] > div[data-check=false]").each(function(){
+                                if (data[i].info_only)
+                                {
+                                    $(this).attr('hidden', data[i].info_only);
+                                }
+                            });
+                            
+                        }
+                        if(currentLength != null && data.length + currentLength > 0)
+                        {
+                            $('#notify').text(data.length + currentLength);
+                            $('#notify').show();
+                        }
+                        
                         
                     }).done(poll).fail(function(data){
-                        var w = window.open();
-                        $(w.document.body).html(data.responseText);
+
+                        if(data.responseText.length > 0)
+                        {
+                            var w = window.open();
+                            $(w.document.body).html(data.responseText);
+                        }
+                        
                     });
                 }, 5000);
             })();
+
+            $(document).on('click', 'div[name=notification]', function(){
+                var url = 'http://192.168.99.100/action/' + $(this).data('notification-id') + '/' + $(this).data('check');
+                var dom = $(this);
+
+                $.get(url, function(data){
+                    dom.closest('li').remove();
+                    var currentLength = $('ul#myMenu li').length;
+
+                    if(currentLength > 0 && currentLength != null)
+                    {
+                        $('#notify').text(currentLength);
+                        $('#notify').show();
+                    }else
+                    {
+                        $('#notify').hide();
+                    }
+
+
+                }).fail(function(data){
+                    alert(data.toString() +  " FAILED");
+                });
+            });
 
             $('#notifications').on('click', function(){
 
