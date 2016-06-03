ad12b00287abc06f5a2182606d8a8e74ead97059
simke9445 <simke9445@gmail.com>
2016-06-03 22:33:05 +0200

    Refactoring Joins Model

--- app/Join.php
+++ app/Join.php
@@ -21,19 +21,19 @@ class Join extends Model
 
     public function accept() {
         $student = $this->request->student;
-        $student->team_id = Student::find(Auth::user()->id)->team_id;
+        $student->team()->associate(Student::find(Auth::user()->id)->team);
         $student->is_leader = false;
         $student->save();
 
         $this->request->status = "ACCEPTED";
         $this->request->save();
-        Notification::createNotification($this->request_id, $student->user_id , "JOIN ACCEPTED" , true);
+        Notification::createNotification($this->request, $student , "JOIN ACCEPTED" , true, true);
     }
 
     public function deny() {
         $this->request->status = "DENIED";
         $this->request->save();
-        Notification::createNotification($this->request_id, $this->request->student_id , "JOIN DENIED" , true);
+        Notification::createNotification($this->request, $this->request->student , "JOIN DENIED" , true, true);
     }
 
 }
