00bb7bd87ae48f5e484acf99fc61473ce46cf34c
simke9445 <simke9445@gmail.com>
2016-06-04 12:08:08 +0200

    Again, really horrible experience

--- resources/views/students/students.blade.php
+++ resources/views/students/students.blade.php
@@ -1,34 +1,19 @@
-@extends('layouts.app')
-
-@section('details')
-	<li><a href="{{ action('StudentsController@show', [Auth::user()->id]) }}" ><i class="fa fa-btn fa-info"></i>Details</a></li>
-
-	@if ($student->is_leader)
-		<li><a href="{{ url('students/team/delete') }}" ><i class="fa fa-btn fa-info"></i>Disband Team</a></li>
-	@else
-		@if ($student->team)
-			<li><a href="{{ url('students/team/leave', [$student->team->id]) }}" ><i class="fa fa-btn fa-info"></i>Leave Team</a></li>
-		@else
-			<li><a href="{{ url('students/team/create') }}" ><i class="fa fa-btn fa-info"></i>Create Team</a></li>
-		@endif
-	@endif
-	
-@endsection
+@extends('layouts.students')
 
 @section('content')
     <div class='container'>
-	   @foreach ($students as $student)
+	   @foreach ($students as $show_student)
 	   <div class="row">
 	   		<div class="col-md-8 col-md-offset-2">
 	   			<div class="panel panel-default">
 	                    <div class="panel-heading">
-		                    <span> {{ $student->name }} </span>
+		                    <span> {{ $show_student->name }} </span>
 		                    <span class="label label-success text-right">
 		                    	Invite
 		                    </span>
 		                </div>
 		                <div class="panel-body">
-		                	@foreach ($student->roles as $role)
+		                	@foreach ($show_student->roles as $role)
 		                		<div class="label label-default">
 		                			{{ $role->name }}
 		                		</div>
