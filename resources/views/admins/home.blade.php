352a68e58578ea30e97b85969323b12059043732
Jovan Djukic <djukicjovan56@gmail.com>
2016-06-04 17:01:53 +0200

    Modified home view so it looks better.

--- resources/views/admins/home.blade.php
+++ resources/views/admins/home.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{ url('/admins/showAllUsers') }}">Link for editing users</a><br>
-    <a href="{{ action('RolesController@index') }}">Link fore editing roles</a>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title text-center">Admin options</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-6 col-md-offset-5">
+                                <a href="{{ action('AdminsController@showAllUsers') }}">Edit users</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 col-md-offset-5">
+                                <a href="{{ action('RolesController@index') }}">Edit roles</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
