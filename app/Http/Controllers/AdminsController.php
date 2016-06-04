7d19c922cdf7e304e5abbcc80aeb74d0188f7de3
Jovan Djukic <djukicjovan56@gmail.com>
2016-06-05 01:11:22 +0200

    Admins controller now handles all role actions.

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Role;
 use App\Student;
 use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class AdminsController extends Controller
 {
@@ -78,5 +80,85 @@ class AdminsController extends Controller
         $users = User::where('name', '!=', 'admin')->get();
 
         return view('admins.showAllUsers')->with('users', $users);
+    }
+
+    public function getAllRoles()
+    {
+        $roles = Role::all();
+
+        return view('admins.showAllRoles', [
+            'roles' => $roles
+        ]);
+    }
+
+    public function createRole()
+    {
+        return view('admins.createRole');
+    }
+
+    public function storeRole(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:255|unique:roles',
+            'description' => 'required|max:500',
+        ]);
+
+        if($validator->fails()) {
+            return redirect()->action('AdminsController@createRole')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $role = Role::create([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+        ]);
+
+        return view('admins.showRole', [
+            'role' => $role,
+        ]);
+    }
+
+    public function showRole($id)
+    {
+        $role = Role::find($id);
+        return view('admins.showRole', [
+            'role' => $role
+        ]);
+    }
+
+    public function editRole($id)
+    {
+        $role = Role::find($id);
+        return view('admins.editRole', [
+            'role' => $role,
+        ]);
+    }
+
+    public function updateRole(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'description' => 'required|max:500',
+        ]);
+
+        if($validator->fails()) {
+            return redirect()->action('AdminsController@editRole', [$id])
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $role = Role::find($id);
+        $role->name = $request->input('name');
+        $role->description = $request->input('description');
+        $role->save();
+
+        return redirect()->action('AdminsController@getAllRoles');
+    }
+
+    public function destroyRole($id)
+    {
+        Role::destroy($id);
+        return redirect()->action('AdminsController@getAllRoles');
     }
 }
