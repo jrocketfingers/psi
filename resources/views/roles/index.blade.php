76f817cff531a637d2ae8d310535bb880089cb74
Jovan Djukic <djukicjovan56@gmail.com>
2016-06-04 17:05:22 +0200

    Modified view so it looks better.

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,24 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <a href="{{ action('RolesController@create') }}">Add new role</a>
-    </div>
-    <div>
-        <table>
-            @foreach($roles as $role)
-                <tr>
-                    <td>{{ $role->name }}</td>
-                    <td><a href="{{ action('RolesController@show', [$role->id]) }}">Show details</a></td>
-                    <td><a href="{{ action('RolesController@edit', [$role->id]) }}">Edit</a></td>
-                    <td>
-                        <form class="form-control" role="form" method="POST" action="{{ url('roles/destroy/'.$role->id) }}" >
-                            {!! csrf_field() !!}
-                            <input type="submit" value="Delete">
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h3 class="panel-title text-center">Roles</h3>
+                            </div>
+                            <div class="col-lg-3 pull-right">
+                                <a href="{{ action('AdminsController@index') }}">Back</a>
+                            </div>
+                            <div class="col-lg-3 pull-right">
+                                <a href="{{ action('RolesController@create') }}">Add new role</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        @foreach($roles as $role)
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    {{ $role->name }}
+                                </div>
+                                <div class="col-lg-2 pull-right">
+                                    <form  role="form" method="POST" action="{{ url('roles/destroy/'.$role->id) }}" >
+                                        {!! csrf_field() !!}
+                                        <input type="submit" value="Delete">
+                                    </form>
+                                </div>
+                                <div class="col-lg-3 pull-right">
+                                    <a href="{{ action('RolesController@show', [$role->id]) }}">Show Details</a>
+                                </div>
+                            </div>
+                            <br>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
