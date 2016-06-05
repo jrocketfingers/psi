ed4a8cbb170b6e3e83f268f23ca45af9246cfac2
Jovan Djukic <djukicjovan56@gmail.com>
2016-06-05 11:44:09 +0200

    Deleted invocation of UsersController show method because we have StudentsController show method.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,6 @@
 @section('content')
 <div class="container">
 
-    <!-- <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/users/destroy/'.Auth::user()->id) }}">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
-                        <button type="submit" class="btn btn-primary">
-                            <i class="fa fa-btn fa-user"></i>Delete account
-                        </button>
-                    </form>
-                    <br>
-                    <a href="{{ action('UsersController@show', [Auth::user()->id]) }}" >Details</a>
-                    <br>
-                    @yield('user_content')
-                </div>
-            </div>
-        </div>
-    </div> -->
     @foreach ($teams as $team)
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
