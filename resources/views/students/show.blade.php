26a5856c5d9703d8a233f747cb546c572ae7d0df
simke9445 <simke9445@gmail.com>
2016-06-06 01:37:09 +0200

    Updating students show details view

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -7,7 +7,7 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">
 					<div class="row">
-						<div class="col-lg-6">
+						<div class="col-lg-5">
 							<h3 class="panel-title">Profile Info</h3>
 						</div>
 
@@ -23,6 +23,11 @@
 
 							<div class="col-lg-3 pull-right">
 								<a href="{{ action('LeaderChangesController@create', [$show_student->user->id]) }}">Promote Leader Vote</a>
+							</div>
+						@endif
+						@if(!$show_student->team && !($show_student->team == $student->team) && $student->team && $student->is_leader)
+							<div class="col-lg-1 pull-right">
+								<a href="{{ action('LeaderChangesController@create', [$show_student->user->id]) }}">Invite</a>
 							</div>
 						@endif
 						
@@ -62,7 +67,7 @@
 						<div class="col-md-8 col-md-offset-2">
 							<div class="row" style="text-align: center;">
 								@foreach($show_student->roles as $role)
-									<div class="label label-default">
+									<div class="label label-default" style="margin-left:0.5em; margin-right:0.5em;">
 										{{ $role->name }}
 									</div>
 						        @endforeach
