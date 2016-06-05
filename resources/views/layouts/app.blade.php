16c0de5a40977f8e25ad637b59907569c2796c75
Jovan Djukic <djukicjovan56@gmail.com>
2016-06-05 12:28:08 +0200

    Fixed.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,6 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
-
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                                 @yield('details')
@@ -92,7 +91,6 @@
                                         </a>
                                     </form>
                                 </li>
-                                
                             </ul>
                         </li>
                     @endif
