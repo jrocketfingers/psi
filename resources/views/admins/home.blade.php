ebfb6e2a7a9448ba7346afb8936b3cfb38748075
simke9445 <simke9445@gmail.com>
2016-06-05 23:20:48 +0200

    Updating admin home view

--- resources/views/admins/home.blade.php
+++ resources/views/admins/home.blade.php
@@ -9,24 +9,24 @@
                         <h3 class="panel-title text-center">Admin options</h3>
                     </div>
                     <div class="panel-body">
-                        <div class="row">
-                            <div class="col-md-6 col-md-offset-5">
-                                <a href="{{ action('AdminsController@showAllUsers') }}" class="label label-primary">Edit users</a>
+                        <div class="row" style="padding-bottom: 0.5em;">
+                            <div class="col-md-4 col-md-offset-4">
+                                <a href="{{ action('AdminsController@showAllUsers') }}" class="btn btn-primary btn-block">Edit users</a>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6 col-md-offset-5">
-                                <a href="{{ action('AdminsController@getAllRoles') }}" class="label label-primary">Edit roles</a>
+                        <div class="row" style="padding-bottom: 0.5em;">
+                            <div class="col-md-4 col-md-offset-4">
+                                <a href="{{ action('AdminsController@getAllRoles') }}" class="btn btn-primary btn-block">Edit roles</a>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6 col-md-offset-5">
-                                <a href="{{ action('AdminsController@showAllNotifications') }}" class="label label-primary">Notification log</a>
+                        <div class="row" style="padding-bottom: 0.5em;">
+                            <div class="col-md-4 col-md-offset-4 text-center">
+                                <a href="{{ action('AdminsController@showAllNotifications') }}" class="btn btn-primary btn-block">Notification log</a>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6 col-md-offset-5">
-                                <a href="{{ action('AdminsController@showAllRequests') }}" class="label label-primary">Request log</a>
+                        <div class="row" style="padding-bottom: 0.5em;">
+                            <div class="col-md-4 col-md-offset-4 text-center">
+                                <a href="{{ action('AdminsController@showAllRequests') }}" class="btn btn-primary btn-block">Request log</a>
                             </div>
                         </div>
                     </div>
