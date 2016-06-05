6ffa1886ec2ecf6fa90902fd2358b93caa9e0991
simke9445 <simke9445@gmail.com>
2016-06-05 19:48:31 +0200

    Add Teams list View

---
+++ resources/views/students/teams.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.students')
+
+@section('content')
+    <div class='container'>
+    	<div class="row" style="padding-bottom: 2em;">
+    		<div class="col-md-8 col-md-offset-2">
+    			{{ Form::open([ 'action' => 'StudentsController@showTeams', 'method' => 'GET']) }}
+
+    			<div class="form-group">
+    				<div class="col-md-2">
+	    				{{ Form::label('Search', 'Search', ['style' => 'margin-top: 0.25em;', 'class' => 'control-label']) }}
+	    			</div>
+	    			<div class="col-md-4">
+	    				{{ Form::text('search', $search, [ 'class' => 'form-control']) }}
+	    			</div>
+
+	    			<div class="col-md-2">
+	    				{{ Form::label('Sort', 'Sort Criteria', ['style' => 'margin-top: 0.25em;', 'class' => 'control-label']) }}
+	    			</div>
+
+	    			<div class="col-md-4">
+	    				{{ Form::select('choice', $choices, null, array('class' => 'form-control')) }}
+	    			</div>
+    			</div>
+
+    			{{ Form::close() }}
+    		</div>
+    	</div>
+	   @foreach ($teams as $team)
+	        <div class="row">
+	            <div class="col-lg-8 col-lg-offset-2">
+	                <div class="panel panel-default">
+	                    <div class="panel-heading">
+		                    <div class="row">
+		                    	<div class="col-lg-2">
+									{{ $team->name }}
+									@if (!($student->team))
+			                    	<a class="label label-success" href="{{ action('JoinsController@create', [$team->id]) }}"> Join </a>
+									@endif
+			                    </div>
+		                        
+		                        @foreach ($team->students as $team_student)
+		                        	<div class="col-lg-1 col-lg-offset-1">
+		                        		<a class="label label-default" href="{{ action('StudentsController@show', [$team_student->user->id]) }}">
+			                        		{{ $team_student->user->name }}
+			                        	</a>
+		                        	</div>
+		                        @endforeach
+		                    </div>
+		                    
+	                    </div>
+	                    <div class="panel-body">
+	                        <label>{{ $team->project_name }}</label>
+	                        <p> {{ $team->description }} </p>
+
+	                        @foreach ($team->roles as $role)
+	                        	<div class="col-lg-1 col-lg-offset-1">
+	                        		<label class="label label-default">
+		                        		{{ $role->name }}
+		                        	</label>
+	                        	</div>
+	                        @endforeach
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    @endforeach
+    </div>
+@endsection
