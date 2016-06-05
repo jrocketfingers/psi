a5f7b626cb23365ff7a72735419d0af2eceab962
Jovan Djukic <djukicjovan56@gmail.com>
2016-06-05 18:41:14 +0200

    Edited edit view so it can upload files.

--- resources/views/assistants/edit.blade.php
+++ resources/views/assistants/edit.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Edit</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ action('AssistantsController@updateDetails') }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ action('AssistantsController@updateDetails') }}" enctype="multipart/form-data">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
@@ -33,6 +33,20 @@
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                     <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Image:</label>
+
+                            <div class="col-md-6">
+                                <input type="file" class="form-control" name="image" id="image">
+
+                                @if ($errors->has('image'))
+                                    <span class="help-block">
+                                    <strong>{{ $errors->first('image') }}</strong>
                                 </span>
                                 @endif
                             </div>
