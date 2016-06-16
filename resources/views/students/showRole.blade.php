60c106e86addda55891b12968557c31b52bd1a8f
Johnny Rocketfingers <developer94@gmail.com>
2016-06-17 00:11:12 +0200

    Add student role description page.

---
+++ resources/views/students/showRole.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.assistants')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                            <h3 class="panel-title text-center">Role</h3>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-lg-offset-2">
+                            <div class="row">
+                                <div class="col-lg-4 col-lg-offset-2">
+                                    <span>Name:</span>
+                                </div>
+                                <div class="col-lg-4 col-lg-offset-2">
+                                    {{ $role->name }}
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4 col-lg-offset-2">
+                                    <span>Description:</span>
+                                </div>
+                                <div class="col-lg-4 col-lg-offset-2">
+                                    {{ $role->description }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
