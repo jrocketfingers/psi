d6ac1f6d5a246648f6ec17594076cbc95b2dd0ce
simke9445 <simke9445@gmail.com>
2016-06-17 14:12:18 +0200

    Add Request test skeletons

---
+++ tests/JoinRequestTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class JoinRequestTest extends TestCase
+{
+    public function testAcceptRequest()
+    {
+
+    }
+
+    public function testDenyRequest()
+    {
+
+    }
+}
