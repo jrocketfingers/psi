ba7039ab75e9d2f55928b1d22b569092b7fdaa6e
simke9445 <simke9445@gmail.com>
2016-06-17 14:14:12 +0200

    Add Assistants login test, and skeleton for the rest

--- tests/AssistantsTest.php
+++ tests/AssistantsTest.php
@@ -4,15 +4,52 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\User;
+use App\Student;
+use App\Assistant;
+use App\Role;
+use App\Team;
+use Faker\Factory as Faker;
+
 class AssistantsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testRegistration()
     {
-        $this->assertTrue(true);
+        $mock = factory(App\User::class)->make();
+
+    	$this->visit('/')
+    		 ->click('Register')
+    		 ->seePageIs('/register')
+    		 ->type($mock->name, 'name')
+    		 ->type($mock->email, 'email')
+    		 ->type($mock->password, 'password')
+    		 ->type($mock->password, 'password_confirmation')
+    		 ->type(csrf_token(), '_token')
+    		 ->select('assistent', 'user_type')
+    		 ->press('Submit')
+    		 ->seeInDatabase('users', [ 'email' => $mock->email ]);
+
+    	$assistant = Assistant::whereHas('user', function($query) use ($mock) { 
+                        $query->where('email', 'like', $mock->email);
+                    })->firstOrFail();
+
+        $assistant->user->delete();
     }
+
+
+    public function testStudentDetailsShow()
+    {
+
+    }
+
+    public function testTeamDetailsShow()
+    {
+
+    }
+
+    public function testTeamDetailsStudentDetailsShow()
+    {
+    	
+    }
+
 }
