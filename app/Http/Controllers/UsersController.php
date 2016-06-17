a869072e918a922a4d7325637e4296687c636c69
Johnny Rocketfingers <developer94@gmail.com>
2016-06-17 15:22:45 +0200

    Remove redundant UsersController methods.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,54 +22,6 @@ class UsersController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-;        $users = User::where('name', '<>', 'admin')->get();
-        return view('users.index')->with('users', $users);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|user:unique',
-        ]);
-
-        if($validator->fails()) {
-            return redirect('users/'.$request->input('id').'/edit')
-                ->withErrors($validator)
-                ->withInput();
-        }
-
-        $user = User::find($request->input('id'));
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->save();
-
-        return redirect()->action('UsersController@show', [$request->input('id')]);
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -82,31 +34,6 @@ class UsersController extends Controller
         return view('users.show', [
             'user' => $user,
         ]);
-
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $user = User::find($id);
-        return view('users.edit')->with('user', $user);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
     }
 
     /**
