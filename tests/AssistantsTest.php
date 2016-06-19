ca2861d09a1f07c41cafac681c30416ed10af415
simke9445 <simke9445@gmail.com>
2016-06-19 17:56:30 +0200

    Add various Assistants requests

--- tests/AssistantsTest.php
+++ tests/AssistantsTest.php
@@ -17,19 +17,19 @@ class AssistantsTest extends TestCase
     {
         $mock = factory(App\User::class)->make();
 
-    	$this->visit('/')
-    		 ->click('Register')
-    		 ->seePageIs('/register')
-    		 ->type($mock->name, 'name')
-    		 ->type($mock->email, 'email')
-    		 ->type($mock->password, 'password')
-    		 ->type($mock->password, 'password_confirmation')
-    		 ->type(csrf_token(), '_token')
-    		 ->select('assistent', 'user_type')
-    		 ->press('Submit')
-    		 ->seeInDatabase('users', [ 'email' => $mock->email ]);
+        $this->visit('/')
+             ->click('Register')
+             ->seePageIs('/register')
+             ->type($mock->name, 'name')
+             ->type($mock->email, 'email')
+             ->type($mock->password, 'password')
+             ->type($mock->password, 'password_confirmation')
+             ->type(csrf_token(), '_token')
+             ->select('assistent', 'user_type')
+             ->press('Submit')
+             ->seeInDatabase('users', [ 'email' => $mock->email ]);
 
-    	$assistant = Assistant::whereHas('user', function($query) use ($mock) { 
+        $assistant = Assistant::whereHas('user', function($query) use ($mock) { 
                         $query->where('email', 'like', $mock->email);
                     })->firstOrFail();
 
@@ -39,17 +39,47 @@ class AssistantsTest extends TestCase
 
     public function testStudentDetailsShow()
     {
+        $user = factory(App\User::class)->create();
+        $student = factory(App\Student::class)->create(['user_id' => $user->id]);
+        $student = Student::find($user->id);
+
+        $user = factory(App\User::class)->create();
+        $assistant = factory(App\Assistant::class)->create([ 'user_id' => $user->id ]);
+        $assistant = Assistant::find($user->id);
+
+        $this->actingAs($assistant->user);
+
+        $response = $this->call('GET', '/assistants/showStudentDetails/'.$student->user->id)->original;
+        $this->assertViewHas('student');
+        $test = $response['student'];
+
+        $this->assertEquals($student->user->id, $test->user->id);    
+
+        $student->user->delete();
+        $assistant->user->delete();
 
     }
 
     public function testTeamDetailsShow()
     {
+        $team = factory(App\Team::class)->create();
+
+        $user = factory(App\User::class)->create();
+        $assistant = factory(App\Assistant::class)->create([ 'user_id' => $user->id ]);
+        $assistant = Assistant::find($user->id);
+
+        $this->actingAs($assistant->user);
+
+        $response = $this->call('GET', '/assistants/showTeamDetails/'.$team->id)->original;
+        $this->assertViewHas('team');
+        $test = $response['team'];
+
+        $this->assertEquals($team->id, $test->id);    
+
+        $team->delete();
+        $assistant->user->delete();
 
     }
 
-    public function testTeamDetailsStudentDetailsShow()
-    {
-    	
-    }
 
 }
